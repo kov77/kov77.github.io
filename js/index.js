57e42c806bb450a1dbc47803bbf14d522da7cb29
Maksym <maksym6167@gmail.com>
2020-05-17 20:42:39 +0200

    post

--- js/index.js
+++ js/index.js
@@ -89,14 +89,15 @@ $(document).ready(function(){
   $('form').submit(function(e) {
     e.preventDefault();
     $.ajax({
-      type: 'POST',
+      type: "POST",
       url: "mailer/smart.php",
       data: $(this).serialize()
     }).done(function() {
         $(this).find('input').val('');
-
+        $('#consultation, #order').fadeOut();
+        $('.overlay, #thanks').fadeIn('slow');
         $('form').trigger('reset');
-      })
+      });
     return false;
     
   })
--- mailer/smart.php
+++ mailer/smart.php
@@ -18,7 +18,7 @@ $mail->Password = 'Q1w2e3r4t5';                           // Наш пароль
 $mail->SMTPSecure = 'ssl';                            // Enable TLS encryption, `ssl` also accepted
 $mail->Port = 465;                                    // TCP port to connect to
  
-$mail->setFrom('Maksym Koval', 'Pulse');   // От кого письмо 
+$mail->setFrom('maksim-koval-90@mail.ru', 'Pulse');   // От кого письмо 
 $mail->addAddress('mehanig@mail.ru');     // Add a recipient
 //$mail->addAddress('ellen@example.com');               // Name is optional
 //$mail->addReplyTo('info@example.com', 'Information');
